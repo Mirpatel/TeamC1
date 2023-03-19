7e5c42bb2a3761451dc26d9e9f9a3223c4aaf0af
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-03-19 16:25:31 -0400

    ggtriesBackend

---
+++ Server/middlewares/authJwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  console.log("veryfiy TOKEN");
+
+  if (!token) {
+    console.log("no token");
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.email = decoded.id;
+    next();
+  });
+};
+const authJwt = {
+    verifyToken,
+  };
+  module.exports = authJwt;
