e89d26eb71b23481b7a74aff3fc303fe9fa24f8e
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-03-22 01:57:43 -0400

    crypto require

--- Server/post.js
+++ Server/post.js
@@ -1,56 +1,4 @@
-/*
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-app.use(cors());
-app.use(express.json());
 
-const db = mysql.createConnection({
-
-    user: 'root',
-    host: 'localhost',
-    password: '',
-    database: 'group'
-    
-});
-
-app.post("/", async (req, res) => {
-  
-  const email = req.body.email;
-  const Password = req.body.Password;
-  
-
-  db.query(
-    "SELECT * FROM user WHERE email = ? AND password = ?",
-    [email, Password],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("An error occurred while processing your request.");
-      }
-
-      if (result.length === 0) {
-        return res.status(401).send("Invalid email or password.");
-      }
-
-      const user = result[0];
-      
-      if (user.role === 'admin') {
-        return res.send({ redirectTo: '/admin' });
-      } else {
-        return res.send({ redirectTo: '/' });
-      }
-    }
-  );
-});
-
-  
-app.listen(8080, () => {
-console.log("running");
-});
-*/
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
@@ -66,7 +14,7 @@ const { PostContactInfo } = require('sib-api-v3-sdk');
 const { response } = require('express');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
-
+const crypto = require('crypto');
 
 
 app.use((req, res, next) => {
