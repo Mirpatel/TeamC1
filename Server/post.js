3afc4f711a4fc1f0872aaca82152dc3503ff5876
Mirpatel <82921562+Mirpatel@users.noreply.github.com>
2023-03-21 20:26:08 -0400

    Update post.js

--- Server/post.js
+++ Server/post.js
@@ -1,14 +1,72 @@
+/*
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'group'
+    
+});
+
+app.post("/", async (req, res) => {
+  
+  const email = req.body.email;
+  const Password = req.body.Password;
+  
+
+  db.query(
+    "SELECT * FROM user WHERE email = ? AND password = ?",
+    [email, Password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send("An error occurred while processing your request.");
+      }
+
+      if (result.length === 0) {
+        return res.status(401).send("Invalid email or password.");
+      }
+
+      const user = result[0];
+      
+      if (user.role === 'admin') {
+        return res.send({ redirectTo: '/admin' });
+      } else {
+        return res.send({ redirectTo: '/' });
+      }
+    }
+  );
+});
+
+  
+app.listen(8080, () => {
+console.log("running");
+});
+*/
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+const bcrypt = require('bcrypt');
+
 var jwt = require("jsonwebtoken");
 const config = require("./config/auth.config");
 const fetch = require('node-fetch');
 const SibApiV3Sdk = require('sib-api-v3-sdk');
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
+
+
+
 app.use((req, res, next) => {
   try {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -19,10 +77,13 @@ app.use((req, res, next) => {
     res.status(500).send('Internal server error');
   }
 });
+
 app.use(cors());
 app.use(express.json());
+
 require('./user.routes')(app);
 const db = mysql.createConnection({
+
     user: 'root',
     host: 'localhost',
     password: '',
@@ -50,8 +111,12 @@ db.query(
 );
 });
 */
+
+
 app.post('/send-profile-email', (req, res) => {
+
   defaultClient.basePath = 'https://api.sendinblue.com/v3';
+
 // Create an instance of the API class
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 let email = req.body.email;
@@ -64,6 +129,7 @@ const sendSmtpEmail = {
     FIRSTNAME: name, //
   },
 };
+
 // Send email
 apiInstance.sendTransacEmail(sendSmtpEmail)
   .then((data) => {
@@ -72,84 +138,56 @@ apiInstance.sendTransacEmail(sendSmtpEmail)
   .catch((error) => {
     console.error(error);
   });
+
+
 });
+
 app.post('/send-password-reset-email', (req, res) => {
 defaultClient.basePath = 'https://api.sendinblue.com/v3';
-
-// make unique link and timestamp
-const token = crypto.randomBytes(20).toString('hex');
-const timestamp = Date.now() + 3600000; // 1 hour from now
-
-// STORE TOKEN / TIMESTAMP IN DATABASE HERE. Use db.Query maybe? Help!!!
 
 // Create an instance of the API class
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 let email = req.body.email;
-	@@ -103,7 +109,7 @@ const sendSmtpEmail = {
+let name = req.body.name;
+// Set email parameters
+const sendSmtpEmail = {
+  to: [{ email: email }],
   templateId: 2, 
   params: {
     FIRSTNAME: name,
-    SMS: 'http://localhost:3000/reset-password/${token}' //
+    SMS: 'link here' //
   },
 };
 
-	@@ -122,6 +128,12 @@ apiInstance.sendTransacEmail(sendSmtpEmail)
-app.post('/send-verify-email', (req, res) => {
-  defaultClient.basePath = 'https://api.sendinblue.com/v3';
+// Send email
+apiInstance.sendTransacEmail(sendSmtpEmail)
+  .then((data) => {
+    console.log('API called successfully. Returned data: ', data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-  // make unique link and timestamp
-  const token = crypto.randomBytes(20).toString('hex');
-  const timestamp = Date.now() + 3600000; // 1 hour from now
-
-  // STORE TOKEN / TIMESTAMP IN DATABASE HERE. Use db.Query maybe? Help!!!
-
-  // Create an instance of the API class
-  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-  let email = req.body.email;
-	@@ -132,7 +144,7 @@ app.post('/send-verify-email', (req, res) => {
-    templateId: 3, 
-    params: {
-      FIRSTNAME: name,
-      SMS: 'http://localhost:3000/signin/${token}' //
-    },
-  };
-
-	@@ -153,7 +165,49 @@ app.post('/send-promotion-email', (req, res) => {
 
 });
 
- app.post('/reset-password/:token', (req, res) => {
-    const { password } = req.body;
-    const { token } = req.params;
-
-    // Retrieve the user's email address and timestamp from your database using the token
-    const user = getUserFromDatabase(token);
-    const { email, timestamp } = user;
-
-    if (timestamp < Date.now()) {
-      res.status(400).send('Password reset link has expired');
-    } else {
-      // Update the user's password in your database
-      updateUserPasswordInDatabase(email, password);
-      res.send('Password reset successful');
-    }
-  });
-
-  // Function to get the user's email, timestamp, and serial key from the database.
-  function getUserFromDatabase(token) {
-    // Retrieve the user's email address and timestamp from your database using the token
-    // I don't know how to retrieve the user's email and timestamp from the database. Help Gigi / Mir!!!
-    // Return an object with the email and timestamp values
-    return {
-      email: 'user@example.com',
-      timestamp: Date.now() + 3600000 // 1 hour from now
-    };
-  }
-
-  function updateUserPasswordInDatabase(email, password) {
-    // Update the user's password in your database. I don't know how to do this, help  Gigi / Mir!!!
-  }
-
+app.post('/send-verify-email', (req, res) => {
+  defaultClient.basePath = 'https://api.sendinblue.com/v3';
+  
+  // Create an instance of the API class
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+  let email = req.body.email;
+  let name = req.body.name;
+  // Set email parameters
+  const sendSmtpEmail = {
+    to: [{ email: email }],
+    templateId: 3, 
+    params: {
+      FIRSTNAME: name,
+      SMS: 'link here' //
+    },
+  };
+  
   // Send email
   apiInstance.sendTransacEmail(sendSmtpEmail)
     .then((data) => {
@@ -158,16 +196,27 @@ app.post('/send-verify-email', (req, res) => {
     .catch((error) => {
       console.error(error);
     });
-
-
+  
+  
   });
+
+app.post('/send-promotion-email', (req, res) => {
+
+
+});
+
+
 
 app.post("/", (req, res) => {
     const email = req.body.email;
     const Password = req.body.Password;
     const promo = req.body.Promo;
     //putting this here to test token
+
     //gonna take away soon
+
+
+
     if (!email || !Password) {
       return res.status(400).send("Email and password are required.");
     }
@@ -184,6 +233,7 @@ app.post("/", (req, res) => {
         if (result.length === 0) {
           return res.status(401).send("Invalid email or password.");
         }
+
         if (promo) {
         var token = jwt.sign({ id: email }, config.secret, {
           expiresIn:  2592000 // 30 days
@@ -196,11 +246,14 @@ app.post("/", (req, res) => {
       });
     }
         const user = result[0];
-      
+      console.log(token);
       if (user.role === 'admin') {
-        return res.send({ redirectTo: '/admin' });
+        return res.send({ redirectTo: '/admin', accessToken: token
+      });
+
       } else {
-        return res.send({ redirectTo: '/' });
+        return res.send({ redirectTo: '/',  accessToken: token
+      });
       }
         console.log("assigned token");
         console.log(token);
@@ -211,11 +264,13 @@ app.post("/", (req, res) => {
           "x-access-token, Origin, Content-Type, Accept"
         );
         res.send("Login successful.", {
-          accessToken: token
         });
       }
     );
+
+
   });
+
   
 app.listen(8080, () => {
 console.log("running on 8080");
